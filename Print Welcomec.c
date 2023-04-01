3a7b542702bd44ff4d74faa239bf6d6b2bcc9251
SiddhiKashid07 <127197226+SiddhiKashid07@users.noreply.github.com>
2023-04-01 21:56:20 +0530

    Programs

---
+++ Print Welcomec.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Cnt = 10;
+
+    for(Cnt = 0; Cnt <= 10; Cnt+=5)
+    {
+        printf("\n %d Welcome",Cnt+101);
+    }
+
+    getch();
+    return 0;
+}
